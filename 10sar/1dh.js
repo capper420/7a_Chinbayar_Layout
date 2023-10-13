6ccecf6b089c6405678b2251632037170dbc98e5
capper420 <127177831+capper420@users.noreply.github.com>
2023-10-13 03:28:13 +0000

    i just learn js

---
+++ 10sar/1dh.js
@@ -0,0 +1,7 @@
+function TakeInput(){
+    let Gmail = document.getElementById("Gmail").value;
+    let Password = document.getElementById("Password").value;
+    console.log("Gmail: ",Gmail,"Password: ",Password);
+    
+
+}
