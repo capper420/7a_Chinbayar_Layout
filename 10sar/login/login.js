529c2abd8f7ef347acdf229d25d191601a86c745
capper420 <127177831+capper420@users.noreply.github.com>
2023-10-20 03:21:08 +0000

    i made anotherc lagin

---
+++ 10sar/login/login.js
@@ -0,0 +1,16 @@
+function Login(){
+    let name = document.getElementById("name").value;
+    let password = document.getElementById("password").value;
+   
+    if (password.length < 8){
+                alert("Password has to be longer than 8");                }  
+    else if ( word[0] =! word[0].CheckUpperCase() ){
+        alert("Username has to be a capital letter");
+    }                          
+     else if (name=="Admin" && password == "12345678"){
+        alert("Login Success");       
+    }  
+    else{
+        alert("Incorrect Username or Password");
+    }
+}
