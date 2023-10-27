7cba00b5310df72945d387189be51719bdac5c6f
capper420 <127177831+capper420@users.noreply.github.com>
2023-10-27 03:21:34 +0000

    done my job

--- 10sar/1dh.js
+++ 10sar/1dh.js
@@ -2,6 +2,6 @@ function TakeInput(){
     let Gmail = document.getElementById("Gmail").value;
     let Password = document.getElementById("Password").value;
     console.log("Gmail: ",Gmail,"Password: ",Password);
-    
+    if(Gmail === bataa@gmail.com && )
 
 }
