3659f6c7c4a250dcc60a5fda50f3a7e5bea0250c
capper420 <127177831+capper420@users.noreply.github.com>
2024-09-24 05:08:36 +0000

    ez

--- 10sar/1dh.js
+++ 10sar/1dh.js
@@ -2,6 +2,13 @@ function TakeInput(){
     let Gmail = document.getElementById("Gmail").value;
     let Password = document.getElementById("Password").value;
     console.log("Gmail: ",Gmail,"Password: ",Password);
-    if(Gmail === bataa@gmail.com && )
+    if(Gmail === "bataa@gmail.com" && Password === "12345678"){
+        alert("login complete");
+        
+    }
+    else{
+        alert("fail");
+    }
 
 }
+
